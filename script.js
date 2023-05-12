70393f248d888fa1483044ba9eb472da8bde6aa2
goshgarsalim <goshgarsalim@gmail.com>
2023-05-12 20:43:44 +0400

    overlay

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let cart_btn = document.querySelector('.cart-btn');
 let mini_cart = document.querySelector('.shop-minicart');
 let close_cart = document.querySelector('.shop-minicart-close');
-
+let overlay_cart = document.querySelector('.overlay-cart');
 
 
 cart_btn.addEventListener("click", () =>{
     mini_cart.classList.add('active-cart');
-})
+});
+cart_btn.addEventListener("click", () =>{
+    overlay_cart.classList.add('overlay-active');
+});
+close_cart.addEventListener("click", () =>{
+    overlay_cart.classList.remove('overlay-active');
+});
 close_cart.addEventListener("click", () =>{
     mini_cart.classList.remove('active-cart');
-})
-
+});
+overlay_cart.addEventListener("click", () =>{
+    mini_cart.classList.remove('active-cart');
+});
+overlay_cart.addEventListener("click", () =>{
+    overlay_cart.classList.remove('overlay-active');
+});
 
